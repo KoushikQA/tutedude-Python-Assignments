ae2c92efa96f93e4ac92a840d23bd15503ec9ec6
palkoushik2025-ML <palkoushik2025@gmail.com>
2025-10-08 20:12:27 +0530

    Commited code for Module 2 Assignment Tasks
    Commited code for Module 2 Assignment Tasks

---
+++ module2_task1.py
@@ -0,0 +1,15 @@
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+addition=num1+num2
+print('Addition: ', addition)
+
+subtraction=num1-num2
+print('Subtraction: ', subtraction)
+
+multiplication=num1*num2
+print('Multiplication', multiplication)
+
+division=num1/num2
+print('Division', division)
+
