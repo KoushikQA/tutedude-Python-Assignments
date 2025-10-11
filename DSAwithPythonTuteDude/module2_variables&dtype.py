e78ae8bb45c6e8a58ec92b0e2dcfeeddc70390b1
KOUSHIK PAL <111008211+KoushikQA@users.noreply.github.com>
2025-10-11 18:41:34 +0530

    Module 2 Assignment Task Completed

---
+++ DSAwithPythonTuteDude/module2_variables&dtype.py
@@ -0,0 +1,78 @@
+'''
+myvar = 19
+print(myvar, type(myvar))
+
+myvar2 = 23.5
+print(myvar2, type(myvar2))
+
+myvar3 = 3 + 5j
+print(myvar3, type(myvar3))
+
+myvar4 = "abcd"
+myvar5 = 'xyz'
+print(myvar4, type(myvar4))
+print(myvar5, type(myvar5))
+'''
+#myvar6 = '''xyz
+#         abc
+#         dyz'''
+#print(myvar6, type(myvar6))
+'''
+myvar7 = [10, 8.2, 13, "abcd"]
+print(myvar7, type(myvar7))
+myVar8 = input()
+print(myVar8, type(myVar8))
+
+myVar9 = 10
+print(myVar9, id(myVar9))
+myVar10 = 10
+print(myVar10, id(myVar10))
+myVar10=19
+print("After update: ")
+print(myVar9, id(myVar9))
+print(myVar10, id(myVar10))
+'''
+'''
+a = 4
+b = 2
+print("a+b: ",a+b)
+print("a-b: ",a-b)
+print("a*b: ",a*b)
+print("a/b: ",a/b)
+print("a**b: ",a**b)
+print("a%b: ",a%b)
+print("a//b: ",a//b)
+
+z=100
+z+=1
+print("z+= ", z)
+z=100
+z-=1
+print("z-= ",z)
+z=100
+z*=1
+print("z*= ",z)
+z=100
+z/=1
+print("z/= ",z)
+z=100
+z**=1
+print("z**= ",z)
+z=100
+z%=1
+print("z%= ",z)
+z=100
+z//=1
+print("z//= ",z)
+'''
+print(3==3)
+print(4==3)
+
+print(3!=3)
+print(4!=3)
+
+print(3==3 or 4==7)
+print(3==3 and 4==7)
+
+print(not 5 > 3)
+print(not 5 > 7)
